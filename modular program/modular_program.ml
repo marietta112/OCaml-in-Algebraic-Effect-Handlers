4bebaa3c4eab9928b97ee26639cf8a254b0a7d5f
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-03-18 08:25:41 +0100

    Renaming of variables, removed redundant comments

--- modular program/modular_program.ml
+++ modular program/modular_program.ml
@@ -1,15 +1,9 @@
-(* open Instr
-open Monitor *)
-open Effect
-open Effect.Deep
 open Effects
 
-(* type _ Effect.t += Put_found : int -> unit Effect.t *)
-
-let piece_of_memory = ref 0
-let put n = piece_of_memory := n; perform(Effects.E.Put_found n)
+let mem = ref 0
+let put n = mem := n; perform(Effects.E.Put n)
 (* *Modifies the value of [piece_of_memory] and performs the effect [Put_found] *)
-let get () = !piece_of_memory
+let get () = !mem
 (* *Dereferences [piece_of_memory] to return the value stored. *)
 
 let comp () = put(5); put(6); put(-1); put(100)
