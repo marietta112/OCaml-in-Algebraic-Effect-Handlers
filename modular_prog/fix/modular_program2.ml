fe94d1295a90d7e372bb5f1608c12ae55d731437
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-05 13:49:54 +0200

    Inserted get() in the computation

--- modular_prog/fix/modular_program2.ml
+++ modular_prog/fix/modular_program2.ml
@@ -6,5 +6,6 @@ let put n = mem := n; perform(Utils.E.Put n)
 (* *Modifies the value of [mem] and performs the effect [Put_found] *)
 let get () = !mem
 (* *Dereferences [mem] to return the value stored. *)
+let x = ref 0
 
-let comp () = put(5); put(6); put(-1); put(100)
+let comp () = put(5); x := get (); print_int !x; put(6); put(-1); put(100)
