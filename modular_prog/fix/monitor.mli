ba0688953b5ac9e31b4b52e522963f0b140e48cd
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-09 21:33:57 +0200

    Added comments for monitor's interface

--- modular_prog/fix/monitor.mli
+++ modular_prog/fix/monitor.mli
@@ -1,5 +1,16 @@
+(* A monitor that checks whether the program tried to store a -1 in memory. *)
 val run_mon1 : unit -> unit
+
+(* A monitor that checks that the running total of the integers stored in memory is less than 500. *)
 val run_sum_mon : unit -> unit
+
+(* A monitor that checks that integers stored in memory alternate between odd and even. *)
 val run_temporal_mon : unit -> unit
+
+(* A monitor that stores the previous integer stored in memory and checks whether the [get] operation
+   returns the most recent integer stored in memory. *)
 val run_mon2 : unit  -> unit
+
+(* A monitor that checks that the running total of the integers stored in memory is less than 500
+   and checks whether the [get] operation returns the most recent integer stored in memory. *)
 val run_mon3 : unit  -> unit
