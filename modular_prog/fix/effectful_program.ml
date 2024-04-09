88e17a66a5570c72c94eaa7e866c5e517988fc00
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-09 17:29:18 +0200

    Added an effectful [get] function

--- modular_prog/fix/effectful_program.ml
+++ modular_prog/fix/effectful_program.ml
@@ -1,17 +1,21 @@
 open Utils
 open Effect
 
-(* Variable that acts as a piece of memory to be updated and read from. *)
-let mem = ref 0
+(* Variable that acts as a piece of memory to be updated and read from. It has the value of 
+   [prev] for the case when [get'] if the first operation in [main]. *)
+let mem = ref !(Utils.E.prev)
 
-(* *Modifies the value of [mem] and performs the effect [Put]. *)
+(* Modifies the value of [mem] and performs the effect [Put]. *)
 let put n = mem := n; perform(Utils.E.Put n)
 
-(* *Dereferences [mem] to return the value stored. *)
+(* Dereferences [mem] to return the value stored. *)
 let get () = !mem
+
+(* Acts like [get] but it raises the effect [Get] after dereferencing. *)
+let get' () = !mem; perform(Utils.E.Get (mem, ()))
 
 (* [x] is a temporary variable in the expression [comp], which is a sequence of [put] and [get]. *)
 let main () = let x = (ref 0) in 
-              let comp () = put(199); x := get (); print_int !x; print_endline ""; 
-              put(200); put(110); put(10); x := get (); print_int !x; print_endline ""
+              let comp () = put(199); x := get' (); print_endline ""; 
+              put(200); put(110); put(10); x := get' (); print_endline ""
               in comp ()
