70dc99f06fbcb0cc789ba43c89e6ff29eb1fb5d1
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-09 17:22:13 +0200

    Added new effect [Get] and rearranged comments

--- modular_prog/fix/utils.ml
+++ modular_prog/fix/utils.ml
@@ -1,9 +1,16 @@
 module E = struct
   type _ Effect.t += Put : int -> unit Effect.t
+  
+  (* Effect that takes in a variable of type int ref and the unit, which returns an integer. *)
+  type _ Effect.t += Get : (int ref) * unit -> int Effect.t
 
+  (* Stores the total sum of integers. To be used for the [sum_monitor] only. *)
   let sum : int ref = ref 0
-  (* To be used for the [sum_monitor] only. *)
-
+  
+  (* Indicates whether an odd or event integer has been stored. 
+     To be used for the [temp_monitor] only. *)
   let flag : int ref = ref 2
-  (* To be used for the [temp_monitor] only. *)
+
+  (* Stores the previous integer. *)
+  let prev : int ref = ref 0
 end
