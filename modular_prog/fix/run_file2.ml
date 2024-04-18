5758013215a90f99646401159212536000404438
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-18 15:47:55 +0200

    Added run functions for all monitors

--- modular_prog/fix/run_file2.ml
+++ modular_prog/fix/run_file2.ml
@@ -1,6 +1,6 @@
 open Monitor
 
-(* let run () = Monitor.run_mon1 () *)
-(* let run () = Monitor.run_sum_mon () *)
-(* let run () = Monitor.run_temporal_mon () *)
-let run () = Monitor.run_mon3 ()
+let run1 () = Monitor.run_mon1 ()
+let run2 () = Monitor.run_sum_mon ()
+let run3 () = Monitor.run_mon3 ()
+let run4 () = Monitor.run_temporal_mon ()
