ce40c6ee711c2d990e546403369a67c2b8342ecb
marietta112 <marietta.k.galea.20@um.edu.mt>
2024-04-19 13:19:22 +0200

    Added run function for aggregate sum with locations

--- modular_prog/fix/run_file2.ml
+++ modular_prog/fix/run_file2.ml
@@ -2,5 +2,6 @@ open Monitor
 
 let run1 () = Monitor.run_mon1 ()
 let run2 () = Monitor.run_sum_mon ()
+let run2_a () = Monitor.run_sum_mon2 ()
 let run3 () = Monitor.run_mon3 ()
 let run4 () = Monitor.run_temporal_mon ()
